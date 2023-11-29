77d9d6506bc388925351e9a25c9e87f6b6b55f9c
dhimas <dhimasaya@gmail.com>
2023-11-29 20:01:14 +0700

    Home Fragment done

--- app/src/main/java/com/example/bp2fp/MainActivity.kt
+++ app/src/main/java/com/example/bp2fp/MainActivity.kt
@@ -4,10 +4,50 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNav: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        //fragment
+        val mainFragment=HomeFragment()
+        val journalFragment=JournalFragment()
+        val newsFragment=NewsFragment()
+
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainer,mainFragment)
+            commit()
+
+        }
+
+        currentFragment(mainFragment)
+
+        bottomNav.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home->currentFragment(mainFragment)
+                R.id.journal->currentFragment(journalFragment)
+                R.id.news->currentFragment(newsFragment)
+
+            }
+            true
+        }
+
+
     }
+
+    private fun currentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainer,fragment)
+            commit()
+        }
+
+
+
 }
